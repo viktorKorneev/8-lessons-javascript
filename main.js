fce74ea63c36be905c59bfeb6cf3729e65e3793d
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-06 18:29:52 +0300

    step=20

--- main.js
+++ main.js
@@ -51,12 +51,21 @@ const scores = [60, 50, 60, 58, 54, 54,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
 
+const costs = [.25, .27, .25, .25, .25, .25,
+    .33, .31, .25, .29, .27, .22,
+    .31, .25, .25, .33, .21, .25,
+    .25, .25, .28, .25, .24, .22,
+    .20, .25, .30, .25, .24, .25, 
+    .25, .25, .27, .25, .26, .29];    
+
     // Bubble solution #0 score: 60
     // Bubble solution #1 score: 50
     // Bubble solution #3 score: 60
     // Bubbles tests: 36
     // Highest bubble score: 69
     // Solutions with highest score: #11, #18
+
+    // Bubble Solution #11 is the most cost effective;
 
     // let i = 0
     
@@ -96,6 +105,20 @@ const scores = [60, 50, 60, 58, 54, 54,
         return highScore
        }
 
+       function getMostCostEffectiveSolution(resultArray, costsArray, maxValue) {
+        let index;
+        let cost = 100
+        for (let i = 0; i < resultArray.length; i++) {
+            if(resultArray[i] === maxValue){
+                if(costsArray[i] < cost) {
+                    cost = costsArray[i]
+                    index = i
+                }
+            }
+        }
+        return index
+       }
+
 
     //print scores and get high score   
    const minScore = printAndGetHighScore(scores) 
@@ -109,5 +132,5 @@ const scores = [60, 50, 60, 58, 54, 54,
   const bestSolutions = getBestScores(scores, highScore)
   console.log("Solutions with highest score: " + bestSolutions)
     
-    
+   //get most cost effective solution
     
