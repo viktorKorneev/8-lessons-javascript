44ad0434c1723decc7f4d0614e90dca069db40b8
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-06 14:35:57 +0300

    step-13

--- main.js
+++ main.js
@@ -40,3 +40,20 @@ function makePhrases(param1, param2, param3) {
 }
 
 alert(makePhrases(words4, words2, words3));
+
+
+
+
+const scores = [60, 50, 60, 58, 54, 54,
+    58, 50, 52, 54, 48, 69,
+    34, 55, 51, 52, 44, 51,
+    69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18,
+    41, 53, 55, 61, 51, 44];
+
+    // Bubble solution #0 score: 60
+    // Bubble solution #1 score: 50
+    // Bubble solution #3 score: 60
+    // Bubbles tests: 36
+    // Highest bubble score: 69
+    // Solutions with highest score: #11, #18
