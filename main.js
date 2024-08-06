27a7882bfc660c3c35a44510d1055d715c44c0b3
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-06 14:47:28 +0300

    step-14

--- main.js
+++ main.js
@@ -9,37 +9,37 @@
 // 8 => 52
 // 9 => 54
 
-const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54];
+// const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54];
 
-console.log(scores[0]);
-console.log(scores[2]);
+// console.log(scores[0]);
+// console.log(scores[2]);
 
-const pets = ["dog", "cat", "rat"];
+// const pets = ["dog", "cat", "rat"];
 
-console.log(pets[2]);
-console.log(pets[3]);
+// console.log(pets[2]);
+// console.log(pets[3]);
 
-console.log(pets.length);
-console.log(scores.length);
+// console.log(pets.length);
+// console.log(scores.length);
 
-pets[2] = "parrot";
-console.log(pets);
+// pets[2] = "parrot";
+// console.log(pets);
 
-function getRandomElement(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
-const words2 = ["любит есть", "хочет погрызть", "всегда ищет"];
-const words3 = ["морковку", "макороны", "косточку"];
-const words4 = ["Мой хомяк", "Моя крыса", "Мой енот"]
+// function getRandomElement(array) {
+//   const randomIndex = Math.floor(Math.random() * array.length);
+//   return array[randomIndex];
+// }
+// const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
+// const words2 = ["любит есть", "хочет погрызть", "всегда ищет"];
+// const words3 = ["морковку", "макороны", "косточку"];
+// const words4 = ["Мой хомяк", "Моя крыса", "Мой енот"]
 
-function makePhrases(param1, param2, param3) {
- const result = getRandomElement(param1) + " " + getRandomElement(param2) + " " + getRandomElement(param3);
- return result;
-}
+// function makePhrases(param1, param2, param3) {
+//  const result = getRandomElement(param1) + " " + getRandomElement(param2) + " " + getRandomElement(param3);
+//  return result;
+// }
 
-alert(makePhrases(words4, words2, words3));
+// alert(makePhrases(words4, words2, words3));
 
 
 
@@ -57,3 +57,15 @@ const scores = [60, 50, 60, 58, 54, 54,
     // Bubbles tests: 36
     // Highest bubble score: 69
     // Solutions with highest score: #11, #18
+
+    let i = 0
+    let output
+    while (i < scores.length) {
+        output = "Bubble solution #" + i + " score: " + scores[i]
+        console.log(output);
+        
+        i = i + 1
+    }
+
+    console.log("Bubbles tests: " + scores.length);
+    
